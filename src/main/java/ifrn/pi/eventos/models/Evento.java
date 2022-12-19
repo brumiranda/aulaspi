06937d930acaa375cfaa4992520d96ce648d38b9
Bruna Tatiane Miranda <bruna.t@academico.ifrn.edu.br>
2022-12-19 16:15:03 -0300

    Enviando dados do formulario para a aplicação

--- src/main/java/ifrn/pi/eventos/models/Evento.java
+++ src/main/java/ifrn/pi/eventos/models/Evento.java
@@ -55,4 +55,11 @@ public class Evento {
 		this.horario = horario;
 	}
 
+	@Override
+	public String toString() {
+		return "Evento [id=" + id + ", nome=" + nome + ", local=" + local + ", data=" + data + ", horario=" + horario
+				+ "]";
+	}
+
+	
 }
