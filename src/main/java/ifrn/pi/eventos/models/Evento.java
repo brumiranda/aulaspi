729e43ccbd732760f9de669b60fc6bf19ea196cf
Bruna Tatiane Miranda <bruna.t@academico.ifrn.edu.br>
2022-12-19 15:36:15 -0300

    Configurando classe de modelo Evento

--- src/main/java/ifrn/pi/eventos/models/Evento.java
+++ src/main/java/ifrn/pi/eventos/models/Evento.java
@@ -1,6 +1,23 @@
 package ifrn.pi.eventos.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Evento {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id; 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	private String nome;
 	private String local;
 	private String data;
