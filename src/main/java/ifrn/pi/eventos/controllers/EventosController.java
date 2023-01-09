a856cf93b7b7b4a90bc3cb68232a18a72a7903f1
Bruna Tatiane Miranda <bruna.t@academico.ifrn.edu.br>
2023-01-09 14:08:13 -0300

    Alterações necessarias

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -21,12 +22,12 @@ public class EventosController {
 	@Autowired
 	private EventoRepository er;
 
-	@RequestMapping("/eventos/form")
+	@RequestMapping("/form")
 	public String form() {
 		return "eventos/formEvento";
 	}
 
-	@RequestMapping(path = "/eventos", method = RequestMethod.POST)
+	@PostMapping
 	public String adicionar(Evento evento) {
 
 		System.out.println(evento);
